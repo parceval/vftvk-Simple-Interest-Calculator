e04ad8f78fcdd36296bdaba68833f0853deca204
Karsten Fuhrmann <parceval3000@gmail.com>
2022-01-14 20:45:21 +0100

    yellow highlighting

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function compute()
     var amount = principal + interest;
 
     // show result in result span
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit <span class=\"number\">"+principal+"</span>,\<br\>at an interest rate of <span class=\"number\">"+rate+"%</span>\<br\>You will receive an amount of <span class=\"number\">"+amount+"</span>,\<br\>in the year <span class=\"number\">"+year+"</span>\<br\>"
 }
 
 function updateRate() 
